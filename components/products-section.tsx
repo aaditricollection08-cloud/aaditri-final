106824eaa88c608b33ccc84f8a12e33f2d39606e
aaditricollection08-cloud <aaditri.collection08@gmail.com>
2026-01-29 09:38:07 +0530

    Import bedsheets data in products-section component
    import bedsheets data

--- components/products-section.tsx
+++ components/products-section.tsx
@@ -1,9 +1,1 @@
-<p className="text-xs text-gray-500 mt-1">
-  Bedsheet Size: 6×7 ft (Queen)
-</p>
-<p className="text-xs text-gray-500">
-  Set Includes: 1 Bedsheet + 2 Pillow Covers + 1 Bolster Cover
-</p>
-<p className="text-xs text-gray-500">
-  Material: Glace Cotton
-</p>
+import { bedsheets } from "@/data/bedsheets";
